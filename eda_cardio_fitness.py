bba3f4e24ed558f6518583166bbb022751f94d22
Rhia Singh <rhia.singh@macaulay.cuny.edu>
2022-05-01 14:58:24 -0400

    Violin plot of gender and fitness

--- eda_cardio_fitness.py
+++ eda_cardio_fitness.py
@@ -4,15 +4,23 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import warnings
 warnings.filterwarnings('ignore') # To supress warnings
-sns.set(style="whitegrid") # set the background for the graphs
+sns.set(style="ticks",palette="Paired") # set the background for the graphs
 
 def prelim_data(data_csv):
     dffitness=pd.read_csv(data_csv)
     print(dffitness.info())
+    return dffitness
 
-def main(filename):
-    prelim_data(filename)
+def violin_plot(df,output_path):
+    v = sns.violinplot(x="Gender", y="Fitness", data=df,palette="Set2")
+    fig = v.get_figure()
+    fig.savefig(output_path+"/gender_fitness_distribution.png")
+
+
+def main(filename,delivery_folder):
+    df=prelim_data(filename)
+    violin_plot(df,delivery_folder)
 
 
 if __name__ == "__main__":
-    main("customer-profile/CardioGoodFitness.csv")
+    main("CardioGoodFitness.csv","output")
