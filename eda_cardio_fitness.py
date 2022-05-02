a9d01deaeda097863f89c6aed7b38bdb044c78a1
Rhia Singh <rhia.singh@macaulay.cuny.edu>
2022-05-01 20:37:11 -0400

    Create stacked bar chart to compare products purched by income

--- eda_cardio_fitness.py
+++ eda_cardio_fitness.py
@@ -11,15 +11,43 @@ def prelim_data(data_csv):
     print(dffitness.info())
     return dffitness
 
-def violin_plot(df,output_path):
+def violin_plot_gender_fitness(df,output_path):
     v = sns.violinplot(x="Gender", y="Fitness", data=df)
     fig = v.get_figure()
     fig.savefig(output_path+"/gender_fitness_distribution.png")
 
+def stacked_bar_product(df,output_path):
+    proddf=df[['Product','Income']]
+    proddf['Income_Round']=df.Income.round(decimals=-4)
+
+    product_income=proddf.groupby(['Product','Income_Round'])['Income'].count().reset_index()
+    #Relative frequency as the Column - drop Frequency
+    product_income.rename({'Income':'Freq'},axis=1,inplace=True)
+    product_income.to_csv(output_path+"/prod_inc.csv")
+
+    #Create pivot table with relative frequency
+    table = pd.pivot_table(product_income, values=['Freq'], columns=['Product'], index=['Income_Round'],aggfunc='sum',fill_value=0)
+    table.reset_index(inplace=True)
+    table.to_csv(output_path+"/prod_inc_pivot.csv")
+
+    sns.set()
+    bars=table.set_index('Income_Round').T.plot(kind='bar', stacked=True)
+    plt.xticks((0, 1, 2), ('TM195', 'TM498', 'TM798'))
+    plt.xticks(rotation = 0)
+    fig = bars.get_figure()
+
+    #Remove x axis title
+    #Format Legend
+
+    fig.savefig(output_path+"/stack_product_income.png")
+
+#Pearson-correlation (look at the math)
+#K-means clustering
 
 def main(filename,delivery_folder):
     df=prelim_data(filename)
-    violin_plot(df,delivery_folder)
+    #violin_plot_gender_fitness(df,delivery_folder)
+    stacked_bar_product(df,delivery_folder)
 
 
 if __name__ == "__main__":
